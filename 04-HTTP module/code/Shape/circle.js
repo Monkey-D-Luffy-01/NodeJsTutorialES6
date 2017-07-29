3f4a560b6a7e8c8f3ee4224c44cf6f5fc88e4ed1
Nguyen Duc Hoang <sunlight4d@gmail.com>
2017-07-29 10:51:53 +0700

    update

--- 04-HTTP module/code/Shape/circle.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by hoangnd on 7/28/17.
- */
-const  {PI} = Math
-module.exports = {
-    area: (radius) => PI * radius * radius,
-}
--- 04-HTTP module/code/index.js
+++ 04-HTTP module/code/index.js
@@ -1,18 +1,25 @@
 /**
  * Created by hoangnd on 7/27/17.
- * Each file is 1 module
+ * HTTP module
  * sunlight4d@gmail.com - Nguyen Duc Hoang
  */
-const rect = require('./Shape/rectangle');
-console.log(`The area of the rectangle(width = 10, height = 20) is ${rect.area(10, 20)}`);
-console.log(`The circumference of the rectangle(width = 10, height = 20) is ${rect.circumference(10, 20)}`);
-console.log(`current time is: ${rect.currentDateTime}`);
-console.log(`directory name: ${rect.directoryName}`);
-// console.log(`directory name: ${rect.__dirname}`);//cannot call like this !
-console.log(`file name: ${rect.fileName}`);
-let aCircle = require('./Shape/circle');
-console.log(`aCircle's area: ${aCircle.area(5)}`);
+var http = require('http');
+var url = require('url');
 
+const server = http.createServer((request, response) => {
+    const ip = request.socket.remoteAddress;
+    const port = request.socket.remotePort;
+    response.end(`Your IP address is ${ip} and port: ${port}.`);
+}).listen(3001);
+
+http.get({
+    hostname: 'localhost',
+    port: 80,
+    path: '/',
+    agent: false  // create a new agent just for this one request
+}, (res) => {
+
+});
 
 
         
